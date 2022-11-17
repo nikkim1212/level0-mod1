9d21a0d098681afcedee1576cc8b5995fd5f0303
nikkim1212 <115122803+nikkim1212@users.noreply.github.com>
2022-11-17 02:31:33 +0000

    Update Remarkable.java

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,17 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+	
+	String Compliment1 = "You are Kind!";
+	String Compliment2 = "You are Funny";
+	String Compliment3 = "You are Helpful";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String username = JOptionPane.showInputDialog("Enter a name: ");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+if(username.equals("Harper")) {
+	JOptionPane.showMessageDialog(null, Compliment1);
+}
 	}
 }
 
